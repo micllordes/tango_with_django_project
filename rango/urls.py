e279d3fc5157df4616b1cfbc81838bf500e50bb6
Michael Llordes <2473352L@student.gla.ac.uk>
2021-01-15 16:57:02 +0100

    Chapter 3 exercises done and tests pass

---
+++ rango/urls.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 15 16:27:17 2021
+
+@author: mic5r
+"""
+from django.urls import path
+from rango import views
+
+app_name= 'rango'
+urlpatterns= [path('',views.index, name='index'), 
+              path('about/', views.about, name='about'),]
